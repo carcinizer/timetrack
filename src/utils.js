6e921b6f114221fedb425ee31e0076693a75f706
Michał Moniewski <mmoniewski@protonmail.com>
2022-07-02 23:43:33 +0200

    Remove unused code

--- src/utils.js
+++ src/utils.js
@@ -95,84 +95,11 @@ function match(tab) {
     }
 }
 
-
 const matchers = {
     item: mapObj(match_items, x=>x.name),
     method(item) {
         return mapObj(match_items[item].methods, x=>x.name)
     }
 }
-
-
-/*
-const matchers = {
-    domain_has: {
-        name: "Domain has",
-        has_url: true,
-        match(entry, tab) {
-            try {
-                if(entry == "about:") {
-                    return true;
-                }
-                return entry.data.length > 0 && new URL(tab.url).hostname.includes(entry.data);
-            }
-            catch {
-                return false;
-            }
-        }
-    },
-    domain_is: {
-        name: "Domain is",
-        has_url: true,
-        match(entry, tab) {
-            try {
-                if(entry == "about:") {
-                    return true;
-                }
-                return entry.data.length > 0 && new URL(tab.url).hostname == entry.data;
-            }
-            catch {
-                return false;
-            }
-        }
-    },
-    url_has: {
-        name: "URL has",
-        has_url: true,
-        match(entry, tab) {
-            try {
-                return entry.data.length > 0 && tab.url.includes(entry.data);
-            }
-            catch {
-                return false;
-            }
-        }
-    },
-    url_is: {
-        name: "URL is",
-        has_url: true,
-        match(entry, tab) {
-            try {
-                return entry.data.length > 0 && tab.url == entry.data;
-            }
-            catch {
-                return false;
-            }
-        }
-    },
-    any: {
-        name: "Any",
-        has_url: false, 
-        match(entry, tab) {
-            return true;
-        }
-    }
-};
-
-
-function match(tab) {
-    return (entry) => matchers[entry.method].match(entry, tab);
-}
-*/
 
 export {dayDuration, getPastResetDate, merge, match, match_items, matchers};
