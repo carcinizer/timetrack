0047e838ca8f59fa61d77be0b7f91ad6811fc10d
Michał Moniewski <mmoniewski@protonmail.com>
2022-07-07 22:51:13 +0200

    Improve timeout popup
    Add more spacing between the message and "Just 5 more minutes..." button
    and use CSS selectors instead of capturing overflow state to make
    scrolling reenabling more consistent

--- src/timeout.js
+++ src/timeout.js
@@ -1,44 +1,50 @@
 
-let overflow = document.body.style.overflow;
 
-let blocker = document.createElement('div')
-blocker.className = "timetrack-timeout-blocker"
+var running = running;
 
-let h = document.createElement('h1');
-h.innerText = "Time's up";
-h.className = "timetrack-timeout-content"
+if(!running) {
+    running = true;
 
-let button = document.createElement('input')
-button.type = 'button';
-button.id = 'timetrack-timeout-more';
-button.value = 'Just 5 more minutes...';
+    let blocker = document.createElement('div')
+    blocker.id = "timetrack-timeout-blocker"
+
+    let h = document.createElement('h1');
+    h.innerText = "Time's up";
+    h.className = "timetrack-timeout-content"
+
+    let button = document.createElement('input')
+    button.type = 'button';
+    button.id = 'timetrack-timeout-more';
+    button.value = 'Just 5 more minutes...';
 
 
-function block() {
-    overflow = document.body.style.overflow;
-    document.body.style.overflow = 'hidden';
-    blocker.hidden = false;
-}
+    const blocker_query = () => document.getElementById('timetrack-timeout-blocker');
 
-function unblock(send) {
-    document.body.style.overflow = overflow;
-    blocker.hidden = true;
-    if(send) {
-        browser.runtime.sendMessage({
-            type: "moreTime", 
-            content: {amount: 5*60*1000}
-        });
+    function block() {
+        blocker_query().hidden = false;
+        document.body.setAttribute('timetrack_block_scrolling', true);
     }
+
+    function unblock(send) {
+        blocker_query().hidden = true;
+        document.body.removeAttribute('timetrack_block_scrolling');
+        if(send) {
+            browser.runtime.sendMessage({
+                type: "moreTime", 
+                content: {amount: 5*60*1000}
+            });
+        }
+    }
+
+
+    blocker.append(h);
+    blocker.append(button);
+    document.body.appendChild(blocker)
+    document.getElementById('timetrack-timeout-more').onclick = () => {unblock(true)}
+
+    block();
+
+    browser.runtime.onMessage.addListener(({should_be_blocked}) => {
+        if(should_be_blocked) {block()} else {unblock(false)}
+    })
 }
-
-
-blocker.append(h);
-blocker.append(button);
-document.body.appendChild(blocker)
-document.getElementById('timetrack-timeout-more').onclick = () => {unblock(true)}
-
-block();
-
-browser.runtime.onMessage.addListener(({should_be_blocked}) => {
-    if(should_be_blocked) {block()} else {unblock(false)}
-})
